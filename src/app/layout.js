6f5c7fdfab1a0ed89f62911248f30516b7e95fc3
renewal398 <renewal398@gmail.com>
2025-09-24 22:56:25 +0100

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "File Share",
+  description: "A simple file sharing app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
